81c32b5915a504ee5a350dda093a67c70397930e
menevia16a <menevia16a@gmail.com>
2021-02-13 03:25:20 -0500

    Slight refactor

--- main.cpp
+++ main.cpp
@@ -26,28 +26,28 @@ int main() {
     RenderWindow window(VideoMode(windowWidth, windowHeight), "Main Menu - VBFP");
     font.loadFromFile("Fonts/arcade.ttf");
 
-    while (true) {
-        window.pollEvent(event);
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+                break;
+            }
 
-        if (event.type == Event::Closed) {
-            window.close();
-            break;
+            if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+                window.close();
+                break;
+            }
+
+            window.clear(Color(128, 128, 128, 255));
+
+            // Set up main menu
+            Text title("Veil's Big Fat Pong", font, 25);
+            title.setPosition(75, 40);
+
+            // Draw main menu
+            window.draw(title);
+            window.display();
         }
-
-        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-            window.close();
-            break;
-        }
-
-        window.clear(Color(128, 128, 128, 255));
-
-        // Set up main menu
-        Text title("Veil's Big Fat Pong", font, 25);
-        title.setPosition(75, 40);
-
-        // Draw main menu
-        window.draw(title);
-        window.display();
     }
 
     //runGameLoop();
@@ -73,76 +73,76 @@ void runGameLoop() {
     hudLives.setFont(font);
     hudLives.setCharacterSize(25);
 
-    while (true) {
-        window.pollEvent(event);
-
-        if (event.type == Event::Closed) {
-            window.close();
-            break;
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::Left)) { bat.moveLeft(); }
-        else if (Keyboard::isKeyPressed(Keyboard::Right)) { bat.moveRight(); }
-        else if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-            window.close();
-            break;
-        }
-
-        // Update frames
-        // *************
-        // Handle the ball hitting the bottom
-        if (ball.getPosition().top > windowHeight) {
-            // Reverse ball direction
-            ball.hitBottom();
-            // Subtract life
-            lives--;
-
-            // Reset game if 0 lives
-            if (lives < 1) {
-                score = 0;
-                lives = 3;
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+                break;
             }
+
+            if (Keyboard::isKeyPressed(Keyboard::Left)) { bat.moveLeft(); }
+            else if (Keyboard::isKeyPressed(Keyboard::Right)) { bat.moveRight(); }
+            else if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+                window.close();
+                break;
+            }
+
+            // Update frames
+            // *************
+            // Handle the ball hitting the bottom
+            if (ball.getPosition().top > windowHeight) {
+                // Reverse ball direction
+                ball.hitBottom();
+                // Subtract life
+                lives--;
+
+                // Reset game if 0 lives
+                if (lives < 1) {
+                    score = 0;
+                    lives = 3;
+                }
+            }
+
+            // Handle the ball hitting the top
+            if (ball.getPosition().top < 0) {
+                ball.reboundBatOrTop();
+                score++;
+            }
+
+            // Handle the ball/bat hitting the sides
+            if (ball.getPosition().left < 0 || ball.getPosition().left + 10 > windowWidth) { ball.reboundSides(); }
+            if (bat.getPosition().left < 0) { bat.collisionLeftSide(); }
+            else if (bat.getPosition().left + 50 > windowWidth) { bat.collisionRightSide(); }
+            // Check if ball hit bat
+            if (ball.getPosition().intersects(bat.getPosition())) { ball.reboundBatOrTop(); }
+
+            // Call updates and set HUD
+            ball.update();
+            bat.update();
+
+            std::stringstream ssScore;
+            std::stringstream ssLives;
+
+            ssScore << "Score:" << score;
+            ssLives << "Lives:" << lives;
+            hudScore.setString(ssScore.str());
+            hudLives.setString(ssLives.str());
+
+            // Draw the frames
+            // ***************
+            // Clear everything from last frame
+            window.clear(Color(0, 128, 255, 255));
+            // Draw bat and ball
+            setResourceTexture(bat.getShape(), BAT_RES);
+            setResourceTexture(ball.getShape(), BALL_RES);
+            window.draw(batShape);
+            window.draw(ballShape);
+            // Draw HUD
+            window.draw(hudScore);
+            window.draw(hudLives);
+            // Display it all
+            window.display();
         }
-
-        // Handle the ball hitting the top
-        if (ball.getPosition().top < 0) {
-            ball.reboundBatOrTop();
-            score++;
-        }
-
-        // Handle the ball/bat hitting the sides
-        if (ball.getPosition().left < 0 || ball.getPosition().left + 10 > windowWidth) { ball.reboundSides(); }
-        if (bat.getPosition().left < 0) { bat.collisionLeftSide(); }
-        else if (bat.getPosition().left + 50 > windowWidth) { bat.collisionRightSide(); }
-        // Check if ball hit bat
-        if (ball.getPosition().intersects(bat.getPosition())) { ball.reboundBatOrTop(); }
-
-        // Call updates and set HUD
-        ball.update();
-        bat.update();
-
-        std::stringstream ssScore;
-        std::stringstream ssLives;
-
-        ssScore << "Score:" << score;
-        ssLives << "Lives:" << lives;
-        hudScore.setString(ssScore.str());
-        hudLives.setString(ssLives.str());
-
-        // Draw the frames
-        // ***************
-        // Clear everything from last frame
-        window.clear(Color(0, 128, 255, 255));
-        // Draw bat and ball
-        setResourceTexture(bat.getShape(), BAT_RES);
-        setResourceTexture(ball.getShape(), BALL_RES);
-        window.draw(batShape);
-        window.draw(ballShape);
-        // Draw HUD
-        window.draw(hudScore);
-        window.draw(hudLives);
-        // Display it all
-        window.display();
     }
 }
 
