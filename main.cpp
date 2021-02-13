d21f77e781b74c45df7c2ad1d12231b971169d1d
menevia16a <menevia16a@gmail.com>
2021-02-13 03:52:43 -0500

    Refactor, and load files from disk once to save resources.

--- main.cpp
+++ main.cpp
@@ -24,7 +24,10 @@ void setResourceTexture(RectangleShape rectShape, Resource resourceId);
 // Game entry point
 int main() {
     RenderWindow window(VideoMode(windowWidth, windowHeight), "Main Menu - VBFP");
+    // Load resources
     font.loadFromFile("Fonts/arcade.ttf");
+    batTex.loadFromFile("Textures/bat.png");
+    ballTex.loadFromFile("Textures/ball.png");
     window.setFramerateLimit(60); // Limit framerate to not have unneeded stress
 
     while (window.isOpen()) {
@@ -75,75 +78,75 @@ void runGameLoop() {
     hudLives.setCharacterSize(25);
 
     while (window.isOpen()) {
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-                break;
-            }
+        window.pollEvent(event);
 
-            if (Keyboard::isKeyPressed(Keyboard::Left)) { bat.moveLeft(); }
-            else if (Keyboard::isKeyPressed(Keyboard::Right)) { bat.moveRight(); }
-            else if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-                window.close();
-                break;
-            }
-
-            // Update frames
-            // *************
-            // Handle the ball hitting the bottom
-            if (ball.getPosition().top > windowHeight) {
-                // Reverse ball direction
-                ball.hitBottom();
-                // Subtract life
-                lives--;
-
-                // Reset game if 0 lives
-                if (lives < 1) {
-                    score = 0;
-                    lives = 3;
-                }
-            }
-
-            // Handle the ball hitting the top
-            if (ball.getPosition().top < 0) {
-                ball.reboundBatOrTop();
-                score++;
-            }
-
-            // Handle the ball/bat hitting the sides
-            if (ball.getPosition().left < 0 || ball.getPosition().left + 10 > windowWidth) { ball.reboundSides(); }
-            if (bat.getPosition().left < 0) { bat.collisionLeftSide(); }
-            else if (bat.getPosition().left + 50 > windowWidth) { bat.collisionRightSide(); }
-            // Check if ball hit bat
-            if (ball.getPosition().intersects(bat.getPosition())) { ball.reboundBatOrTop(); }
-
-            // Call updates and set HUD
-            ball.update();
-            bat.update();
-
-            std::stringstream ssScore;
-            std::stringstream ssLives;
-
-            ssScore << "Score:" << score;
-            ssLives << "Lives:" << lives;
-            hudScore.setString(ssScore.str());
-            hudLives.setString(ssLives.str());
-
-            // Draw the frames
-            // ***************
-            // Clear everything from last frame
-            window.clear(Color(0, 128, 255, 255));
-            // Draw bat and ball
-            setResourceTexture(bat.getShape(), BAT_RES);
-            setResourceTexture(ball.getShape(), BALL_RES);
-            window.draw(batShape);
-            window.draw(ballShape);
-            // Draw HUD
-            window.draw(hudScore);
-            window.draw(hudLives);
-            // Display it all
-            window.display();
+        if (event.type == Event::Closed) {
+            window.close();
+            break;
         }
+
+        if (Keyboard::isKeyPressed(Keyboard::Left)) { bat.moveLeft(); }
+        else if (Keyboard::isKeyPressed(Keyboard::Right)) { bat.moveRight(); }
+        else if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+            break;
+        }
+
+        // Update frames
+        // *************
+        // Handle the ball hitting the bottom
+        if (ball.getPosition().top > windowHeight) {
+            // Reverse ball direction
+            ball.hitBottom();
+            // Subtract life
+            lives--;
+
+            // Reset game if 0 lives
+            if (lives < 1) {
+                score = 0;
+                lives = 3;
+            }
+        }
+
+        // Handle the ball hitting the top
+        if (ball.getPosition().top < 0) {
+            ball.reboundBatOrTop();
+            score++;
+        }
+
+        // Handle the ball/bat hitting the sides
+        if (ball.getPosition().left < 0 || ball.getPosition().left + 10 > windowWidth) { ball.reboundSides(); }
+        if (bat.getPosition().left < 0) { bat.collisionLeftSide(); }
+        else if (bat.getPosition().left + 50 > windowWidth) { bat.collisionRightSide(); }
+        // Check if ball hit bat
+        if (ball.getPosition().intersects(bat.getPosition())) { ball.reboundBatOrTop(); }
+
+        // Call updates and set HUD
+        ball.update();
+        bat.update();
+
+        std::stringstream ssScore;
+        std::stringstream ssLives;
+
+        ssScore << "Score:" << score;
+        ssLives << "Lives:" << lives;
+        hudScore.setString(ssScore.str());
+        hudLives.setString(ssLives.str());
+
+        // Draw the frames
+        // ***************
+        // Clear everything from last frame
+        window.clear(Color(0, 128, 255, 255));
+        // Draw bat and ball
+        setResourceTexture(bat.getShape(), BAT_RES);
+        setResourceTexture(ball.getShape(), BALL_RES);
+        window.draw(batShape);
+        window.draw(ballShape);
+        // Draw HUD
+        window.draw(hudScore);
+        window.draw(hudLives);
+        // Display it all
+        window.display();
     }
 }
 
@@ -151,12 +154,10 @@ void setResourceTexture(RectangleShape rectShape, Resource resourceId) {
     // Set the textures of the game
     if (resourceId == BAT_RES) {
         batShape = rectShape;
-        batTex.loadFromFile("Textures/bat.png");
         batShape.setTexture(&batTex);
     }
     else if (resourceId == BALL_RES) {
         ballShape = rectShape;
-        ballTex.loadFromFile("Textures/ball.png");
         ballShape.setTexture(&ballTex);
     }
 }
